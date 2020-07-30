8d33217212d723157f75b03c56fd8c929e93c439
Jinwei.sun <sunjinwei_11@163.com>
2020-07-30 10:09:22 +0800

    添加父子项目

---
+++ study-spring-boot-starter/src/main/java/com/xiao/study/springbootstarter/StudySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.study.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ study-spring-boot-starter/src/test/java/com/xiao/study/springbootstarter/StudySpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiao.study.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
